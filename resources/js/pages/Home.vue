6fe003718e57090e424b22d29ebcc1035ad6a913
pikas <tautvydaszemaitis1996@gmail.com>
2026-08-13 23:08:10 +0300

    init laravel and vue.js

---
+++ resources/js/pages/Home.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+        <h1>Home</h1>
+        <p>Laravel 13 + Vue 3 + PrimeVue</p>
+    </div>
+</template>
---
+++ resources/views/app.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="lt">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body>
+<div id="app"></div>
+</body>
+</html>
