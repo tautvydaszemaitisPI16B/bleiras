4698597d981a4e733e831c9d387576853f137b1e
pikas <tautvydaszemaitis1996@gmail.com>
2026-08-14 00:41:11 +0300

    add seeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::updateOrCreate(
+            [
+                'email' => 'admin@example.com',
+            ],
+            [
+                'name' => 'Administratorius',
+                'password' => Hash::make('password'),
+            ]
+        );
+    }
+}
